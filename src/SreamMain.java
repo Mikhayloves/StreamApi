1508e5d17064081e76ad341f660d74f95208490d
Neto test <toni_mikhaylovskiy@mail.ru>
2024-01-08 23:05:28 +0300

    Revert "."
    This reverts commit dc659d01bb4f9942d4af2aa0786f4bc0753011c4.

--- src/SreamMain.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class SreamMain {
-      .filter(x -> x > 0)
-            .filter(x -> x % 2 == 0)
-            .sorted(Comparator.naturalOrder())
-            .forEach(System.out::println);
-}
-
